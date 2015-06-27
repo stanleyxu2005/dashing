8e9b39dea542487f40767340c97b2c93b1720527
Qian Xu <sx.away@googlemail.com>
2015-06-28 01:04:38 +0800

    0.0.2 file contents were concat randomly

--- dist/dashing.js
+++ dist/dashing.js
@@ -1,6 +1,8 @@
 /*
- * Licensed under the Apache License, Version 2.0
- * See accompanying LICENSE file.
+ * dashing
+ * @version v0.0.2
+ * @link https://github.com/stanleyxu2005/dashing
+ * @license Apache License 2.0, see accompanying LICENSE file
  */
 (function(window, document, undefined) {
 'use strict';
@@ -16,15 +18,30 @@ angular.module('dashing', [
   'dashing.state',
   'dashing.tabset'
 ])
-;angular.module("dashing").run(["$templateCache", function($templateCache) {$templateCache.put("charts/charts-comp-sparklinechartcomp1.html","<metrics caption=\"{{caption}}\" ng-attr-help=\"{{help}}\" value=\"{{current}}\" unit=\"{{unit}}\"></metrics> <sparkline-chart options-bind=\"options\" datasource-bind=\"data\"></sparkline-chart>");
+;
+angular.module("dashing").run(["$templateCache", function($templateCache) {$templateCache.put("charts/charts-comp-sparklinechartcomp1.html","<metrics caption=\"{{caption}}\" ng-attr-help=\"{{help}}\" value=\"{{current}}\" unit=\"{{unit}}\"></metrics> <sparkline-chart options-bind=\"options\" datasource-bind=\"data\"></sparkline-chart>");
 $templateCache.put("charts/charts-comp-sparklinechartcomp2.html","<div class=\"row\"> <metrics class=\"col-md-4\" style=\"{{metricsStyleFix}}\" caption=\"{{caption}}\" ng-attr-help=\"{{help}}\" value=\"{{current}}\" unit=\"{{unit}}\" small-text=\"{{smallText}}\"></metrics> <sparkline-chart class=\"col-md-offset-1 col-md-7\" options-bind=\"options\" datasource-bind=\"data\"></sparkline-chart> </div>");
 $templateCache.put("metrics/metrics.html","<div class=\"metrics\"> <div> <span ng-bind=\"caption\"></span> <help ng-if=\"help\" text=\"{{help}}\"></help> </div> <h3 class=\"metrics-value\"> <span ng-bind=\"value\"></span> <small ng-bind=\"unit\"></small> </h3> <small ng-if=\"smallText\" class=\"metrics-small-text\" ng-bind=\"smallText\"></small> </div>");
-$templateCache.put("property/property.html","<div ng-switch=\"renderer\">  <a ng-switch-when=\"Link\" ng-href=\"{{href}}\" ng-bind=\"text\"></a>  <state ng-switch-when=\"State\" text=\"{{text}}\" condition=\"{{condition}}\"></state>  <indicator ng-switch-when=\"Indicator\" text=\"{{text}}\" condition=\"{{condition}}\"></indicator>  <progressbar ng-switch-when=\"ProgressBar\" current=\"{{current}}\" max=\"{{max}}\"></progressbar>  <span ng-switch-when=\"Duration\" ng-bind=\"value|duration\"></span>  <span ng-switch-default ng-bind=\"value\"></span> </div>");
 $templateCache.put("progressbar/progressbar.html","<div style=\"width:100%\">  <span class=\"small pull-left\" ng-bind=\"current+\'/\'+max\"></span> <span class=\"small pull-right\" ng-bind=\"usage + \'%\'\"></span> </div> <div style=\"width:100%\" class=\"progress progress-tiny\"> <div ng-class=\"\'progress-bar-\'+colorFn(usage)\" ng-style=\"{width:usage+\'%\'}\" class=\"progress-bar\"></div> </div>");
 $templateCache.put("property-table/property-table.html","<table class=\"table table-striped table-condensed\"> <caption ng-if=\"caption\" ng-bind=\"caption\"></caption> <tbody> <tr ng-repeat=\"prop in props track by $index\"> <td ng-class=\"propNameClass\"> <small ng-bind=\"prop.name\"></small> <help ng-if=\"prop.help\" text=\"{{prop.help}}\"></help> </td> <td ng-switch=\"prop.hasOwnProperty(\'values\')\" ng-class=\"propValueClass\"> <div ng-switch-when=\"true\" ng-repeat=\"value in prop.values track by $index\"> <property value-bind=\"value\" renderer=\"{{prop.renderer}}\"></property> </div> <div ng-switch-default> <property value-bind=\"prop.value\" renderer=\"{{prop.renderer}}\"></property> </div> </td> </tr> </tbody> </table>");
+$templateCache.put("property/property.html","<div ng-switch=\"renderer\">  <a ng-switch-when=\"Link\" ng-href=\"{{href}}\" ng-bind=\"text\"></a>  <state ng-switch-when=\"State\" text=\"{{text}}\" condition=\"{{condition}}\"></state>  <indicator ng-switch-when=\"Indicator\" text=\"{{text}}\" condition=\"{{condition}}\"></indicator>  <progressbar ng-switch-when=\"ProgressBar\" current=\"{{current}}\" max=\"{{max}}\"></progressbar>  <span ng-switch-when=\"Duration\" ng-bind=\"value|duration\"></span>  <span ng-switch-default ng-bind=\"value\"></span> </div>");
 $templateCache.put("sortable-table/sortable-table-pagination.html","<div ng-if=\"pages.length >= 2\"> <div class=\"btn-group btn-group-xs\"> <button type=\"button\" class=\"btn btn-default\" ng-class=\"{disabled: 1==currentPage}\" ng-click=\"selectPage(1)\"> <span class=\"glyphicon glyphicon-step-backward\"></span></button> <button type=\"button\" class=\"btn btn-default\" ng-class=\"{disabled: 1==currentPage}\" ng-click=\"selectPage(currentPage-1)\"> <span class=\"glyphicon glyphicon-chevron-left\"></span></button> <button type=\"button\" class=\"btn btn-default\" ng-repeat=\"page in pages\" ng-class=\"{active: page==currentPage}\" ng-click=\"selectPage(page)\"> {{page}} </button> <button type=\"button\" class=\"btn btn-default\" ng-class=\"{disabled: numPages==currentPage}\" ng-click=\"selectPage(currentPage+1)\"> <span class=\"glyphicon glyphicon-chevron-right\"></span></button> <button type=\"button\" class=\"btn btn-default\" ng-class=\"{disabled: numPages==currentPage}\" ng-click=\"selectPage(numPages)\"> <span class=\"glyphicon glyphicon-step-forward\"></span></button> </div> </div>");
 $templateCache.put("sortable-table/sortable-table.html","<table class=\"table table-striped table-condensed\" st-table=\"records_\" st-safe-src=\"records\"> <caption ng-if=\"caption\" ng-bind=\"caption\"></caption> <thead> <tr> <th ng-repeat=\"col in columns track by $index\" ng-class=\"stylingFn(col)\" ng-attr-st-sort=\"{{col.sortKey}}\" st-sort-default=\"{{col.defaultSort}}\">{{col.name}} <help ng-if=\"col.hasOwnProperty(\'help\')\" text=\"{{col.help}}\"></help> </th> </tr> <tr ng-if=\"searchable\"> <th colspan=\"{{columns.length}}\"> <input type=\"text\" st-search placeholder=\"{{searchable}}\"> </th> </tr> </thead> <tbody> <tr ng-repeat=\"record in records_\"> <td ng-repeat=\"col in columns track by $index\" ng-class=\"stylingFn(col)\"> <div ng-switch=\"isArray(col.key)\"> <div ng-switch-when=\"true\" ng-repeat=\"childKey in col.key track by $index\"> <property value-bind=\"record[childKey]\" renderer=\"{{col.renderer[$index]}}\"></property> </div> <div ng-switch-default> <property value-bind=\"record[col.key]\" renderer=\"{{col.renderer}}\"></property> </div> </div> </td> </tr> <tr ng-if=\"records_.length===0\"> <td colspan=\"{{columns.length}}\" class=\"text-center\"> <span class=\"small\">No data found</span> </td> </tr> </tbody> <tfoot ng-if=\"records_.length>0\"> <tr> <td colspan=\"{{columns.length}}\"> <div class=\"pull-left\"> Total: <span ng-bind=\"records_.length\"></span> </div> <div class=\"pull-right\" st-pagination st-items-by-page=\"pagination\" st-template=\"sortable-table/sortable-table-pagination.html\"></div> </td> </tr> </tfoot> </table>");
-$templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-underlined\"> <li ng-repeat=\"tab in tabs\" ng-class=\"{active:tab.selected}\"> <a href=\"\" ng-click=\"selectTab(tab)\" ng-bind=\"tab.heading\"></a> </li> </ul> <div class=\"tab-content\" ng-transclude></div>");}]);angular.module('dashing.charts-comp', [
+$templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-underlined\"> <li ng-repeat=\"tab in tabs\" ng-class=\"{active:tab.selected}\"> <a href=\"\" ng-click=\"selectTab(tab)\" ng-bind=\"tab.heading\"></a> </li> </ul> <div class=\"tab-content\" ng-transclude></div>");}]);
+angular.module('dashing.help', [
+  'mgcrea.ngStrap.tooltip'
+])
+  .directive('help', function() {
+    return {
+      template: '<span class="glyphicon glyphicon-question-sign help-icon" bs-tooltip="text"></span>',
+      restrict: 'E',
+      scope: {
+        text: '@'
+      }
+    };
+  })
+;
+angular.module('dashing.charts-comp', [
   'dashing.charts',
   'dashing.metrics'
 ])
@@ -58,7 +75,8 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
       }
     };
   })
-;angular.module('dashing.charts', [
+;
+angular.module('dashing.charts', [
   'dashing.metrics'
 ])
   .directive('echart', function() {
@@ -190,19 +208,8 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
       }]
     };
   })
-;angular.module('dashing.help', [
-  'mgcrea.ngStrap.tooltip'
-])
-  .directive('help', function() {
-    return {
-      template: '<span class="glyphicon glyphicon-question-sign help-icon" bs-tooltip="text"></span>',
-      restrict: 'E',
-      scope: {
-        text: '@'
-      }
-    };
-  })
-;angular.module('dashing.metrics', [])
+;
+angular.module('dashing.metrics', [])
   .directive('metrics', function() {
     return {
       templateUrl: 'metrics/metrics.html',
@@ -216,7 +223,8 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
       }
     };
   })
-;angular.module('dashing.progressbar', [])
+;
+angular.module('dashing.progressbar', [])
   .directive('progressbar', function() {
     return {
       templateUrl: 'progressbar/progressbar.html',
@@ -236,7 +244,22 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
       }]
     };
   })
-;angular.module('dashing.property', [])
+;
+angular.module('dashing.property-table', [])
+  .directive('propertyTable', function () {
+    return {
+      templateUrl: 'property-table/property-table.html',
+      restrict: 'E',
+      scope: {
+        caption: '@',
+        props: '=propsBind',
+        propNameClass: '@',
+        propValueClass: '@'
+      }
+    };
+  })
+;
+angular.module('dashing.property', [])
   .directive('property', function() {
     return {
       templateUrl: 'property/property.html',
@@ -304,20 +327,8 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
       }).join(" and ");
     };
   })
-;angular.module('dashing.property-table', [])
-  .directive('propertyTable', function () {
-    return {
-      templateUrl: 'property-table/property-table.html',
-      restrict: 'E',
-      scope: {
-        caption: '@',
-        props: '=propsBind',
-        propNameClass: '@',
-        propValueClass: '@'
-      }
-    };
-  })
-;angular.module('dashing.sortable-table', [
+;
+angular.module('dashing.sortable-table', [
   'smart-table'
 ])
   .directive('sortableTable', function() {
@@ -343,7 +354,8 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
     .config(['stConfig', function(stConfig) {
     stConfig.sort.skipNatural = true;
   }])
-;angular.module('dashing.state', [])
+;
+angular.module('dashing.state', [])
   .directive('state', function() {
     return {
       template: '<span ng-class="stylingFn(condition)" class="label" ng-bind="text"></span>',
@@ -392,7 +404,8 @@ $templateCache.put("tabset/tabset.html","<ul class=\"nav nav-tabs nav-tabs-under
       }]
     };
   })
-;angular.module('dashing.tabset', [])
+;
+angular.module('dashing.tabset', [])
   .directive('dsTabset', [function() {
     return {
       templateUrl: 'tabset/tabset.html',
--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,10 @@
 var brand = 'dashing';
 var header = [
   '/*',
-  ' * Licensed under the Apache License, Version 2.0',
-  ' * See accompanying LICENSE file.',
+  ' * dashing',
+  ' * @version v' + require('./package.json').version,
+  ' * @link https://github.com/stanleyxu2005/dashing',
+  ' * @license Apache License 2.0, see accompanying LICENSE file',
   ' */',
   ''].join('\n')
 var output_dir = 'dist';
@@ -16,10 +18,12 @@ var output_dir = 'dist';
 var gulp = require('gulp');
 var tool = {
   concat: require('gulp-concat-util'),
+  fs: require('fs'),
   htmlmin: require('gulp-htmlmin'),
   minifycss: require('gulp-minify-css'),
   rename: require("gulp-rename"),
   replace: require('gulp-replace'),
+  sort: require('gulp-sort'),
   sourcemaps: require('gulp-sourcemaps'),
   stripcomments: require('gulp-strip-comments'),
   stripemptylines: require('gulp-remove-empty-lines'),
@@ -27,11 +31,14 @@ var tool = {
   uglifyjs: require('gulp-uglify'),
 }
 
-gulp.task('default', ['min-css', 'min-js', 'doc']);
+gulp.task('default', ['min-css', 'min-js', 'doc'], function() {
+  tool.fs.unlink('src/tpls.js');
+});
 
 // concat all css files as one
 gulp.task('concat-css', function() {
   return gulp.src('src/**/*.css')
+    .pipe(tool.sort()) // `gulp.src()` returns filenames in random order
     .pipe(tool.concat(brand + '.css'))
     .pipe(tool.stripcomments())
     .pipe(tool.stripemptylines())
@@ -53,6 +60,7 @@ gulp.task('min-css', ['concat-css'], function() {
 // create angular template cache
 gulp.task('pack-angular-templates', function() {
   return gulp.src('src/**/*.html')
+    .pipe(tool.sort()) // `gulp.src()` returns filenames in random order
     .pipe(tool.htmlmin({removeComments: true, collapseWhitespace: true, conservativeCollapse: true}))
     .pipe(tool.templatecache('tpls.js', {module: brand}))
     .pipe(gulp.dest('src'));
@@ -61,6 +69,13 @@ gulp.task('pack-angular-templates', function() {
 // concat all js files as one
 gulp.task('concat-js', ['pack-angular-templates'], function() {
   return gulp.src('src/**/*.js')
+    .pipe(tool.sort({ // `gulp.src()` returns filenames in random order
+      comparator: function(file1, file2) {
+        var path = require('path');
+        function l(f) { return f.path.split(path.sep).length; }
+        var result = l(file1) - l(file2);
+        return result === 0 ? file1.path > file2.path : result;
+      }}))
     .pipe(tool.concat(brand + '.js'))
     .pipe(tool.stripcomments())
     .pipe(tool.stripemptylines())
@@ -84,20 +99,6 @@ gulp.task('min-js', ['concat-js'], function() {
 
 // copy license and readme
 gulp.task('doc', function() {
-  return gulp.src('LICENSE')
+  return gulp.src(['LICENSE', '*.md'])
     .pipe(gulp.dest(output_dir));
-});
-
-// misc
-gulp.task('misc', function() {
-/*
-  ['src', 'src/modules'].map(function(location) {
-    console.log(location);
-    gulp.src(location + '/*.js')
-      .pipe(tool.replace('gearcomp', 'dashing'))
-      .pipe(tool.replace('templates', 'tpls'))
-      .pipe(gulp.dest(location));
-  });*/
-  return gulp.src(output_dir + '/*.*')
-    .pipe(gulp.dest('../demo/src/vendors/dashing/0.0.1'));
 });

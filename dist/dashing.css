a7f0f0e887d62c2fc7263f48e12cdecbecde0e7e
Qian Xu <sx.away@googlemail.com>
2015-08-24 00:49:22 +0800

    released 0.1.6

--- dist/dashing.css
+++ dist/dashing.css
@@ -1,6 +1,6 @@
 /*
  * dashing (assembled widgets)
- * @version v0.1.5
+ * @version v0.1.6
  * @link https://github.com/stanleyxu2005/dashing
  * @license Apache License 2.0, see accompanying LICENSE file
  */

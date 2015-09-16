24722f04eafdbe1cbcd929d8138c549f311d6c56
Qian Xu <sx.away@googlemail.com>
2015-09-17 00:43:21 +0800

    fix #13: support ring chart with rich title at right

--- src/charts/ring.js
+++ src/charts/ring.js
@@ -20,11 +20,16 @@ angular.module('dashing.charts.ring', [
  * @param options-bind - the option object, which the following elements:
  * {
  *   height: string // the css height of the chart
- *   textPosition: 'inner'|'right' // either show current usage inside the ring or show total value at right (default: inner)
  *   color: string // optional to override ring color
+ *
+ *   title: string // optional to show a title right to the ring. The total value and unit will be show as well.
+ *   thickness: number // optional to override the ratio of inner/outer ring radius (default: 0.25)
  * }
- * @param datasource-bind - array of data objects
- *   every data object is {x: time|string, y: [number]}
+ * @param datasource-bind - data object
+ * {
+     used: {value: 64, label: 'Allocated', unit: 'GB'},
+     available: {value: 256 - 64, label: 'Available'}
+   };
  */
   .directive('ringChart', function() {
     'use strict';
@@ -50,11 +55,16 @@ angular.module('dashing.charts.ring', [
           });
         });
       },
-      controller: ['$scope', '$echarts', function($scope, $echarts) {
+      controller: ['$scope', '$element', '$echarts', function($scope, $element, $echarts) {
         var use = angular.merge({
           color: 'rgb(35,183,229)',
-          textPosition: 'inner'
+          thickness: 0.25
         }, $scope.options);
+
+        if (!angular.isNumber(use.thickness) || use.thickness > 1 || use.thickness <= 0) {
+          console.warn({message: 'Invalid thickness value', value: use.thickness});
+          use.thickness = 0.25;
+        }
 
         var data = use.data || $scope.data;
         if (!data) {
@@ -63,10 +73,15 @@ angular.module('dashing.charts.ring', [
         var colors = $echarts.buildColorStates(use.color);
         var padding = 8;
         var outerRadius = (parseInt(use.height) - 30 - padding * 2) / 2;
+        var innerRadius = Math.floor(outerRadius * (1 - use.thickness));
+        var innerTextFontSize = Math.floor(28 * innerRadius / 39);
+        if (innerTextFontSize < 12) {
+          console.warn('Please increase the height to get a better visual experience.');
+        }
         var itemStyleBase = {
           normal: {
             color: 'rgb(232,239,240)',
-            label: {show: use.textPosition === 'inner', position: 'center'},
+            label: {show: true, position: 'center'},
             labelLine: false
           }
         };
@@ -88,7 +103,7 @@ angular.module('dashing.charts.ring', [
           series: [{
             type: 'pie',
             center: ['50%', outerRadius + padding],
-            radius: [Math.floor(outerRadius * 0.74), outerRadius],
+            radius: [innerRadius, outerRadius],
             data: [{
               name: data.available.label,
               value: data.available.value,
@@ -103,45 +118,57 @@ angular.module('dashing.charts.ring', [
           }]
         };
 
-        switch (use.textPosition) {
-          case 'inner':
-            options.series[0].itemStyle = {
-              normal: {
-                label: {
-                  formatter: function() {
-                    return Math.round($scope.data.used.value * 100 /
-                        ($scope.data.used.value + $scope.data.available.value)) + '%';
-                  },
-                  textStyle: {
-                    color: '#111',
-                    fontSize: 28, // todo: auto-adjust font size?
-                    fontWeight: 500,
-                    baseline: 'middle'
-                  }
-                }
+        options.series[0].itemStyle = {
+          normal: {
+            label: {
+              formatter: function() {
+                return Math.round($scope.data.used.value * 100 /
+                    ($scope.data.used.value + $scope.data.available.value)) + '%';
+              },
+              textStyle: {
+                color: '#111',
+                fontSize: Math.floor(28 * innerRadius / 39),
+                fontWeight: 500,
+                baseline: 'middle'
               }
-            };
-            break;
-          case 'right':
-            if (use.title) {
-              options.series[0].center[0] = outerRadius + padding;
-              options.legend.x = padding;
-              options.title = {
-                text: $scope.data.used.value + ($scope.data.used.unit || ''),
-                subtext: use.title,
-                itemGap: 11,
-                x: (outerRadius + padding) * 2 + padding,
-                y: outerRadius + padding - 40,
-                textStyle: {
-                  fontSize: 40,
-                  fontWeight: 500
-                },
-                subtextStyle: {
-                  fontSize: 14
-                }
-              };
             }
-            break;
+          }
+        };
+
+        if (use.title) {
+          options.series[0].center[0] = outerRadius + padding;
+          options.legend.x = padding;
+          options.title = {
+            text: use.title,
+            x: (outerRadius + padding) * 2 + padding + 4,
+            y: outerRadius + padding + 4,
+            textStyle: {
+              fontSize: 12,
+              fontWeight: 500,
+              color: '#666'
+            }
+          };
+
+          // As Echarts' title does not support, we insert a real HTML element.
+          var left = options.title.x + 14;
+          var top = options.title.y - 48;
+          var total = $scope.data.used.value + $scope.data.available.value;
+          var unit = $scope.data.used.unit;
+          var unselectable =
+            '-webkit-touch-callout: none;' +
+            '-webkit-user-select: none;' +
+            '-khtml-user-select: none;' +
+            '-moz-user-select: none;' +
+            '-ms-user-select: none;' +
+            'user-select: none;';
+          angular.element($element[0]).append([
+            '<div style="position: absolute; left: ' + left + 'px; top: ' + top + 'px">',
+            '<p style="cursor: default; ' + unselectable + '">',
+            '<span style="font-size: 40px; font-weight: 500">' + total + '</span>',
+            (unit ? ('<span style="font-size: 15px; font-weight: 700">' + unit + '</span>') : ''),
+            '</p>',
+            '</div>'
+          ].join(' '));
         }
 
         $scope.echartOptions = options;

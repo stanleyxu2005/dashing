d05800cd58061c9e9cf0450d0c4efa1dccf02488
Qian Xu <sx.away@googlemail.com>
2017-07-08 12:36:04 +0800

    fix #68: when duration is less than 1ms, show text as "<1ms"

--- src/util/text.js
+++ src/util/text.js
@@ -42,12 +42,12 @@ angular.module('dashing.util.text', [])
         }
 
         var units = [
-          {label: ' ms', mod: 1000},
-          {label: compact ? 's' : ' secs', mod: 60},
-          {label: compact ? 'm' : ' mins', mod: 60},
-          {label: compact ? 'h' : ' hours', mod: 24},
-          {label: compact ? 'd' : ' days', mod: 7},
-          {label: compact ? 'w' : ' weeks', mod: 52}
+          {text: compact ? 'ms' : 'msecs', mod: 1000},
+          {text: compact ? 's' : 'secs', mod: 60},
+          {text: compact ? 'm' : 'mins', mod: 60},
+          {text: compact ? 'h' : 'hours', mod: 24},
+          {text: compact ? 'd' : 'days', mod: 7},
+          {text: compact ? 'w' : 'weeks', mod: 52}
         ];
         var duration = [];
 
@@ -55,17 +55,21 @@ angular.module('dashing.util.text', [])
           var unit = units[i];
           var t = x % unit.mod;
           if (t !== 0) {
-            duration.unshift({label: unit.label, value: t});
+            duration.unshift({unit: unit.text, value: t});
           }
           x = (x - t) / unit.mod;
         }
 
         duration = duration.slice(0, 2);
-        if (duration.length > 1 && duration[1].label === ' ms') {
+        var millisUnit = units[0].text
+        if (duration.length > 1 && duration[1].unit === millisUnit) {
+          // Reduce the text length by removing milliseconds part
           duration = [duration[0]];
+        } else if (duration.length === 0) {
+          duration = [{value: '<1', unit: millisUnit}];
         }
-        return duration.map(function(unit) {
-          return unit.value + unit.label;
+        return duration.map(function(p) {
+          return p.value + (compact ? '' : ' ') + p.unit;
         }).join(compact ? ' ' : ' and ');
       }
     };
